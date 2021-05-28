df38f933ce7b3c56072c8609c2c3d910a3c7b9bd
debugger333X <75965411+debugger333X@users.noreply.github.com>
2021-05-28 11:20:02 +0530

    added input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Heyaa</h1>
+      <input type="text" placeholder="Username..." />
     </div>
   );
 }
