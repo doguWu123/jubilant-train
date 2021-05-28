3e7cd37ca78eaa32d975ad0d666d64ad68da9275
debugger333X <75965411+debugger333X@users.noreply.github.com>
2021-05-28 14:14:49 +0530

    a

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <input type="text" placeholder="Username..." />
+      <h1 style={{ color: "white" }}>Sign Up</h1>
+      <p>
+        <input
+          style={{ color: "white" }}
+          type="text"
+          placeholder="username..."
+        />
+      </p>
+      <p>
+        <input type="text" placeholder="email..." />
+      </p>
+      <p>
+        <input type="text" placeholder="password..." />
+      </p>
     </div>
   );
 }
