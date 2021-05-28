1a5ff385f5c09b1d7076173ef2b475bf8408841c
debugger333X <75965411+debugger333X@users.noreply.github.com>
2021-05-28 15:08:11 +0530

    added button and some styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./styles.css";
+import styled from "styled-components";
+
+function Signup() {
+  alert("Signed In!");
+}
 
 export default function App() {
   return (
     <div className="App">
       <h1 style={{ color: "white" }}>Sign Up</h1>
       <p>
-        <input
-          style={{ color: "white" }}
-          type="text"
-          placeholder="username..."
-        />
+        <input type="text" placeholder="username..." />
       </p>
       <p>
         <input type="text" placeholder="email..." />
@@ -17,6 +18,9 @@ export default function App() {
       <p>
         <input type="text" placeholder="password..." />
       </p>
+      <button style={{ color: "white" }} onclick={Signup}>
+        Sign Up!
+      </button>
     </div>
   );
 }
